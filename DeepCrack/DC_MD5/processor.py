39764047b1b1756185a9f6f06197c051ef5e343e
Hu Yao-Chieh <yhuag@connect.ust.hk>
2017-11-20 23:03:11 +0800

    Create distance function: Average bit value for each bit

--- DeepCrack/DC_MD5/processor.py
+++ DeepCrack/DC_MD5/processor.py
@@ -5,10 +5,12 @@ import collections
 
 print("Libraries Imported Successfully")
 
-# Collect all MD5 strings into md5_list
+# Initializations
 md5_list = []
 file_name = 'VirusShare_00000.md5'
+MD5_BIT_LENGTH = 128
 
+# Collect all MD5 strings into md5_list
 with open(file_name) as file:
     for line in file:
         md5_list.append(line.rstrip())  # rstrip gets rid of '\n'
@@ -22,33 +24,49 @@ print("Total Length: " + str(len(md5_list)))
 def md52bitStr(md5_str):
     return bin(int("0x" + md5_str, 16))[2:]
 
-###### Distance Functions ######
+
+
+
+
+###### Distance Functions: Average bit value for each bit ######
+# Find the average bit for each vec
+bit_count_vec = [0] * MD5_BIT_LENGTH
+
+def addBitVec(bin_str):
+    return [x + int(y) for x,y in zip(bit_count_vec, list(bin_str))]
+
+for md5_str in md5_list:
+    bit_count_vec = addBitVec(md52bitStr(md5_str))
+
+bit_count_vec = [x/len(md5_list) for x in bit_count_vec]
+print(bit_count_vec)
+plt.plot(bit_count_vec)
+plt.show()
+
+
+
+
+
+###### Distance Functions: Hamming Distance to 0-vector ######
 def dis0Vec(bin_str):
     return bin_str.count('0')
 
+# # Collect distances into dictionary
+# lib = {}
+
+# for md5_str in md5_list:
+#     dis = dis0Vec(md52bitStr(md5_str))
+#     if dis in lib:
+#         lib[dis] += 1
+#     else:
+#         lib[dis] = 1
 
 
+# # Sort lib
+# sorted_lib = collections.OrderedDict(sorted(lib.items()))
+# print(sorted_lib)
 
-
-
-
-
-# Collect distances into dictionary
-lib = {}
-
-for md5_str in md5_list:
-    dis = dis0Vec(md52bitStr(md5_str))
-    if dis in lib:
-        lib[dis] += 1
-    else:
-        lib[dis] = 1
-
-
-# Sort lib
-sorted_lib = collections.OrderedDict(sorted(lib.items()))
-print(sorted_lib)
-
-# Plot
-# plt.scatter(lib.keys(), lib.values())
-plt.plot(sorted_lib.keys(), sorted_lib.values())
-plt.show()
+# # Plot
+# # plt.scatter(lib.keys(), lib.values())
+# plt.plot(sorted_lib.keys(), sorted_lib.values())
+# plt.show()
